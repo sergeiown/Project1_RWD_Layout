f34646c33b54c81f1778876b29286ac61ca4c1a8
Serhii Myshko <sergeiown@gmail.com>
2022-11-27 11:40:49 +0200

    add mobile menu improvements

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,19 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("overflow-hidden");
+}
+
+navMenu.addEventListener("click", mobileMenuHidden);
+
+function mobileMenuHidden() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  body.classList.remove("overflow-hidden");
 }
